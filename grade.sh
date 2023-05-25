380adc0733203859c22b121294c84cd8bdb73433
AngelinaYee <yyee@ucsd.edu>
2023-05-24 18:43:47 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,9 +8,23 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+set -e
+files=`find list-methods-lab3`
+for file in $files
+do
+if [[ -f $file ]] && [[ $file == *ListExamples* ]]
+then
+wc -l $file
+fi
+done
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+set -e;
+javac Server.java
+javac GradeServer.java
